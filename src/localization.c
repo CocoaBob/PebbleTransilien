3b18a6424a5bbf338c60ddce460df6c666206d03
CocoaBob <mengke.wang@gmail.com>
2015-07-09 01:49:11 +0200

    Added localization.h/.c

---
+++ src/localization.c
@@ -0,0 +1,20 @@
+//
+//  localization.c
+//  PebbleTransilien
+//
+//  Created by CocoaBob on 09/07/15.
+//  Copyright (c) 2015 CocoaBob. All rights reserved.
+//
+
+#include "localization.h"
+
+char *localized_string_with_id(char *string_id) {
+//    if (strcmp("fr_FR", sys_locale) == 0) {
+//        text_layer_set_text(s_output_layer, "Bonjour tout le monde!");
+//    } else if ( /* Next locale string comparison */ ) {
+//        /* other language cases... */
+//    } else {
+//        // Fall back to English
+//        text_layer_set_text(s_output_layer, "Hello, world!");
+//    }
+}
---
+++ src/localization.h
@@ -0,0 +1,11 @@
+//
+//  localization.h
+//  PebbleTransilien
+//
+//  Created by CocoaBob on 09/07/15.
+//  Copyright (c) 2015 CocoaBob. All rights reserved.
+//
+
+#pragma once
+
+char *localized_string_with_id(char *string_id);
