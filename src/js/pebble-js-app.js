1149018303194b858e032bcbc55e8a6172d76404
CocoaBob <mengke.wang@gmail.com>
2015-10-15 15:26:36 +0200

    Some small modifications.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -134,7 +134,7 @@ var next_trains_example = '[\
 "trainDock": "",\
 "trainHour": "22/07/2015 18:31",\
 "trainLane": null,\
-"trainMention": "Retarde",\
+"trainMention": "Retardé",\
 "trainMissionCode": "POBA",\
 "trainNumber": "133758",\
 "trainTerminus": "PSL",\
@@ -144,7 +144,7 @@ var next_trains_example = '[\
 "trainDock": " ",\
 "trainHour": "22/07/2015 18:33",\
 "trainLane": null,\
-"trainMention": "Retarde",\
+"trainMention": "Retardé",\
 "trainMissionCode": "SEBO",\
 "trainNumber": "134657",\
 "trainTerminus": "SNB",\
